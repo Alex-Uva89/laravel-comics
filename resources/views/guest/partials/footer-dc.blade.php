7828d37f81bbe844afa243d46922c5523390f9e1
ALESSANDROUVA <a.uva1988@gmail.com>
2022-04-27 19:01:34 +0200

    Manca footer inferiore

--- resources/views/guest/partials/footer-dc.blade.php
+++ resources/views/guest/partials/footer-dc.blade.php
@@ -1,17 +1,42 @@
 <footer>
     <div class="footer-high">
-        <ul>
-            <li>digital comics</li>
-            <li>dc merchandise</li>
-            <li>subscription</li>
-            <li>comic shop locator</li>
-            <li>dc power visa</li>
+        <ul class="nav-footer">
+            <li>
+                <a href="http://">
+                    <img src="/images/buy-comics-digital-comics.png" alt="">
+                    digital comics
+                </a>
+            </li>
+            <li>
+                <a href="http://">
+                    <img src="/images/buy-comics-merchandise.png" alt="">
+                    dc merchandise
+                </a>
+            </li>
+            <li>
+                <a href="http://">
+                    <img src="/images/buy-comics-subscriptions.png" alt="">
+                    subscription
+                </a>
+            </li>
+            <li>
+                <a href="http://">
+                    <img src="/images/buy-comics-shop-locator.png" alt="">
+                    comic shop locator
+                </a>
+            </li>
+            <li>
+                <a href="http://">
+                    <img src="/images/buy-dc-power-visa.svg" alt="">
+                    dc power visa
+                </a>
+            </li>
         </ul>
     </div>
 
     <div class="footer-middle">
         <ul>
-
+            
         </ul>
     </div>
 
