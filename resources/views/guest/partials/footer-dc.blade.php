c1fe31429e8d866e430249d1927542e29a8027d9
ALESSANDROUVA <a.uva1988@gmail.com>
2022-04-27 19:26:00 +0200

    completo manca active header

--- resources/views/guest/partials/footer-dc.blade.php
+++ resources/views/guest/partials/footer-dc.blade.php
@@ -35,12 +35,67 @@
     </div>
 
     <div class="footer-middle">
-        <ul>
-            
-        </ul>
+            <div class="container-lists">
+              <ul class="list">
+                  <li><h3>Dc Comics</h3></li>
+                  <li ><a href="#">Link 1</a></li>
+                  <li ><a href="#">Link 2</a></li>
+                  <li ><a href="#">Link 3</a></li>
+                  <li ><a href="#">Link 4</a></li>
+                  <li ><a href="#">Link 5</a></li>
+              </ul>
+              <ul class="list">
+                  <li><h3>Shop</h3></li>
+                  <li ><a href="#">Link 1</a></li>
+                  <li ><a href="#">Link 2</a></li>
+                  <li ><a href="#">Link 3</a></li>
+                  <li ><a href="#">Link 4</a></li>
+                  <li ><a href="#">Link 5</a></li>
+              </ul>
+              <ul class="list">
+                  <li><h3>Dc</h3></li>
+                  <li ><a href="#">Link 1</a></li>
+                  <li ><a href="#">Link 2</a></li>
+                  <li ><a href="#">Link 3</a></li>
+                  <li ><a href="#">Link 4</a></li>
+                  <li ><a href="#">Link 5</a></li>
+              </ul>
+              <ul class="list">
+                  <li><h3>Sites</h3></li>
+                  <li ><a href="#">Link 1</a></li>
+                  <li ><a href="#">Link 2</a></li>
+                  <li ><a href="#">Link 3</a></li>
+                  <li ><a href="#">Link 4</a></li>
+                  <li ><a href="#">Link 5</a></li>
+              </ul>
+            </div>
+      
+            <div>
+                <img src="/images/dc-logo-bg.png" alt="">
+            </div>
     </div>
 
-    <div class="footer-low"></div>
+    <div class="footer-low">
+        <button>Sign-up</button>
+        <ul class="list">
+           <li>follow us</li>
+           <li>
+               <img src="/images/footer-facebook.png" alt="Facebook">
+           </li>
+           <li>
+               <img src="/images/footer-periscope.png" alt="Periscope">
+           </li>
+           <li>
+               <img src="/images/footer-pinterest.png" alt="Pinterest">
+           </li>
+           <li>
+               <img src="/images/footer-twitter.png" alt="Twitter">
+           </li>
+           <li>
+               <img src="/images/footer-youtube.png" alt="Toutube">
+           </li>
+        </ul>
+    </div>
 </footer>
 
 
