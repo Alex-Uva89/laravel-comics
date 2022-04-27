db0c43e6694752b04aef1854f9b8df6caaff9aab
ALESSANDROUVA <a.uva1988@gmail.com>
2022-04-27 15:54:27 +0200

    config

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $comics = config('comics');
-
+    $comics = config('comics')
     $data = [
         'comics' => $comics
     ];
+
     return view('guest.home');
 })->name('home');
 
