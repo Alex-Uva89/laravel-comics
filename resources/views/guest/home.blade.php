ba792d57f0d3b473c101f25c49cfc0c720586c3d
ALESSANDROUVA <a.uva1988@gmail.com>
2022-04-27 18:03:48 +0200

    continuo css

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,11 +3,15 @@
 @section('title', 'DC - Home')
 
 @section('content')
+<div class="container-background">
 <div class="jumbotron"></div>
-
-<div>
-    @foreach ($comics as $comic)
-        <img src="{{ $comic['thumb'] }}" alt="">
-    @endforeach
+    <div class="container-card">
+        @foreach ($comics as $comic)
+            <div class="card">
+                <img src="{{ $comic['thumb'] }}" alt="{{$comic['title']}} img">
+                <h3>{{ $comic['title'] }}</h3>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
